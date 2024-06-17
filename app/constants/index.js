1a22b8f0dfe1b503323f1dac9e4f786d459565ce
Rushad <rushadmahmed@gmail.com>
2024-06-17 15:16:47 +0530

    Project Config

---
+++ app/constants/index.js
@@ -0,0 +1,9 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Home" },
+  { href: "/", key: "reviews", label: "Reviews" },
+  { href: "/", key: "services", label: "Plan a Trip With Us" },
+  { href: "/", key: "about", label: "About" },
+];
+
+export const REVIEW_COUNT = 51;
