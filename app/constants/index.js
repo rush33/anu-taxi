6661a5ecbe98882377752da6193e63046c62a941
Anuran Saikia <niyonlite@gmail.com>
2024-06-17 16:47:04 +0530

    Added Reviews and profile pics

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,3 +1,5 @@
+import { userAgent } from "next/server";
+
 // NAVIGATION
 export const NAV_LINKS = [
   { href: "/", key: "home", label: "Home" },
@@ -6,13 +8,32 @@ export const NAV_LINKS = [
   { href: "/", key: "about", label: "About" },
 ];
 
-export const REVIEW_COUNT = 51;
+export const REVIEW_COUNT = 54;
 
 export const REVIEWS = [
   {
-    username: "neil",
+    username: "aneesh ss",
+    content:
+      "We had a 5 day trip to Meghalaya in May 24. We engaged Anu Tours and Travels for the vehicle requirement for the trip. I should submit that the agency has provided an excellent support for our trip. The vehicle was good. Driver was very knowledgeable, friendly and met all our requirements. In fact he advised us correctly regarding each place and guided us based on our age group, visit requirement and reaching various points in time there by ensuring our itinerary is completely covered. Anu Tours and Travels is highly recommended for any trip from guwahati.",
+  },
+  {
+    username: "Umashankar Pattnam",
     content:
       "During our recent visit to Meghalaya, we engaged services of Anu Tour and Taxi from Gauhati. The entire trip was smooth and satisfying . Our Driver Joy Deb made our travel more comfortable with his polite, fun loving behaviour. He was never in aggressive drive mode, even in empty roads, thereby ensuring the safety. He understood our requirements of good veg food and took us only to that sort of places, which never fails to satisfy anyone. Recommend , travellers to use their services to have a good and comfortable travel experience in North East.",
   },
-  { username: "neil", content: "Home" },
+  {
+    username: "Raj Shekhar",
+    content:
+      "Excellent service. We enquired a travel plan to Meghalaya from Anu Tour and Taxi and got it in reasonable rates. Mr Joydeb was very cordial and also helped us in curating the itinerary. Our driver (Ajit da) was also polite and knowledgeable about the area. The trip went very smooth and we had a very good experience.",
+  },
+  {
+    username: "Rohit Patwi",
+    content:
+      "Anu tour and taxi service in Guwahati is excellent. The owner Mr. Joydeb Das is very nice, humble and professional person. His safe driving skills, on time services and knowledge of North east areas are amazing. He has good network for arranging trip and food stops. I recommend anyone visiting Assam, Meghalaya and Arunachal Pradesh to avail their services.",
+  },
+  {
+    username: "Nimesh Chidrawar",
+    content:
+      "I was traveling solo for first time, my luck i got Anu tours and travels. The owner booked all hotels for me, took me to pure veg hotels, literally did trekking with me on high hills. Just close your eyes and book. You will have joyful memories. Very responsible, humble, caring person he is ….🙏🏻 Thanks a lot 😇",
+  },
 ];
