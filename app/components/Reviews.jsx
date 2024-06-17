ca4eec12abb0f9304e93bf7520815f9bc307ae40
Rushad <rushadmahmed@gmail.com>
2024-06-17 16:13:18 +0530

    Updated Reviews Section

--- app/components/Reviews.jsx
+++ app/components/Reviews.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { REVIEWS } from "./../constants/index";
+
+const ReviewCard = ({ content, username }) => {
+  return (
+    <div className="bg-green-50 m-4 p-8 lg:max-w-[450px] xl:max-w-[734px] xl:rounded-5xl xl:px-14 xl:py-16 w-full relative overflow-hidden rounded-3xl">
+      <div className="flexCenter flex items-center gap-3 mb-3">
+        {Array(5)
+          .fill(1)
+          .map((_, index) => (
+            <Image
+              src="/star.svg"
+              key={index}
+              alt="star"
+              width={24}
+              height={24}
+            />
+          ))}
+      </div>
+      <p className="regular-16 xl:regular-16  text-white">{content}</p>
+      <Image
+        src="/quote.svg"
+        alt="quote"
+        width={186}
+        height={219}
+        className="camp-quote"
+      />
+      <div className="flexCenter mt-3">
+        <Image
+          src={`/${username}.png`}
+          alt="Client Avatar"
+          className="rounded-full mr-2"
+          width={50}
+          height={50}
+        />
+        <p className="bold-20 text-white">{username}</p>
+      </div>
+    </div>
+  );
+};
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
-  )
-}
+    <section className="max-container bg-white py-20">
+      <div className="mx-auto p-10 max-w-[133rem]">
+        <div className="flex flex-col">
+          <div className="text-center text-black">
+            <h3 className="regular-20">Reviewed by People</h3>
+            <h2 className="bold-40">Client's Testimonials</h2>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row">
+        {REVIEWS.map((item, index) => (
+          <ReviewCard
+            key={index}
+            content={item.content}
+            username={item.username}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Reviews
+export default Reviews;
--- app/constants/index.js
+++ app/constants/index.js
@@ -7,3 +7,12 @@ export const NAV_LINKS = [
 ];
 
 export const REVIEW_COUNT = 51;
+
+export const REVIEWS = [
+  {
+    username: "neil",
+    content:
+      "During our recent visit to Meghalaya, we engaged services of Anu Tour and Taxi from Gauhati. The entire trip was smooth and satisfying . Our Driver Joy Deb made our travel more comfortable with his polite, fun loving behaviour. He was never in aggressive drive mode, even in empty roads, thereby ensuring the safety. He understood our requirements of good veg food and took us only to that sort of places, which never fails to satisfy anyone. Recommend , travellers to use their services to have a good and comfortable travel experience in North East.",
+  },
+  { username: "neil", content: "Home" },
+];
