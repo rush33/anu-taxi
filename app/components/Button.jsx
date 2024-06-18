d7320d6b58d78faf0f4a2748e6511e5704713bea
Rushad <rushadmahmed@gmail.com>
2024-06-18 16:45:52 +0530

    Added Functionality to Button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
-const Button = ({ type, title, icon, variant }) => {
+const Button = ({ type, title, icon, variant, onClick }) => {
   return (
     <button
       type={type}
       className={`flexCenter gap-3 rounded-full border ${variant} cursor-pointer`}
+      onClick={onClick}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <p className="bold-16 whitespace-nowrap">{title}</p>
