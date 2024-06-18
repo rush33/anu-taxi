2521011deefc257e4794db1ed8f8864949b62e8c
Rushad <rushadmahmed@gmail.com>
2024-06-18 16:46:42 +0530

    Hero Section uses Constants

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,12 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import Button from "./Button";
-import { REVIEW_COUNT } from "../constants";
+import { HERO_SUBTITLE, HERO_TITLE, PHONE_NUMBER, REVIEW_COUNT, WHATSAPP_MESSAGE, WHATSAPP_NUMBER } from "../constants";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Hero = () => {
+  var settings = {
+    dots: true,
+    lazyLoad: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 1,
+  };
+
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
-
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         {/* <Image
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px] "
@@ -16,10 +29,9 @@ const Hero = () => {
           height={50}
         /> */}
 
-        <h1 className="bold-52 lg:bold-88">Save big with our car rental</h1>
+        <h1 className="bold-52 lg:bold-88">{HERO_TITLE}</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          Rent the car of your dreams. Unbeatable prices, unlimited miles,
-          flexible pick-up options and much more.
+          {HERO_SUBTITLE}
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
@@ -46,17 +58,87 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
-          <Button title="Book A Ride" type="button" variant="btn_green" />
           <Button
-            title="How We Work?"
-            icon="/play.svg"
+            title="Call Now"
+            icon="/call.svg"
             type="button"
-            variant="btn_white_text"
+            variant="btn_green"
+            onClick={() => (window.location.href = `tel:${PHONE_NUMBER}`)}
+          />
+
+          <Button
+            title="WhatsApp"
+            icon="/whatsapp-green.svg"
+            type="button"
+            variant="btn_white"
+            onClick={() =>
+              window.open(
+                `https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`,
+                "_blank"
+              )
+            }
+          />
+
+          {/* <IconButton icon="/whatsapp.svg" type="button" /> */}
+        </div>
+
+        {/*TODO: Photos Carousel */}
+
+        {/* <div className="">
+          <Image
+            src="/1.jpeg"
+            alt="photos"
+            width={300}
+            height={300}
+            objectFit="cover"
           />
         </div>
+        <div>
+          <Image
+            src="/2.jpeg"
+            alt="photos"
+            width={300}
+            height={300}
+            objectFit="cover"
+          />
+        </div>
+        <div>
+          <Image
+            src="/3.jpeg"
+            alt="photos"
+            width={300}
+            height={300}
+            objectFit="cover"
+          />
+        </div>
+        <div>
+          <Image
+            src="/4.jpeg"
+            alt="photos"
+            width={300}
+            height={300}
+            objectFit="cover"
+          />
+        </div>
+        <div>
+          <Image
+            src="/5.jpeg"
+            alt="photos"
+            width={300}
+            height={300}
+            objectFit="cover"
+          />
+        </div>
+        <div>
+          <Image
+            src="/6.jpeg"
+            alt="photos"
+            width={300}
+            height={300}
+            objectFit="cover"
+          />
+        </div> */}
       </div>
-
-      {/*TODO: Photos Carousel */}
     </section>
   );
 };
