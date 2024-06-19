b4fac46c37388c3bb9c13cb28672ab3df10d666f
Rushad <rushadmahmed@gmail.com>
2024-06-19 13:54:09 +0530

    Updated site description

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Anu Tours and Taxi",
-  description: "Anu Tours and Taxi by Rushad and Anuran",
+  description: "Arunachal & Meghalaya taxi service from Guwahati",
 };
 
 export default function RootLayout({ children }) {
