bdd86f4aefe70435be199a6ae51ff2f8abb3ba98
Rushad <rushadmahmed@gmail.com>
2024-06-22 11:57:14 +0530

    Fixed font responsiveness

--- app/components/PlanTrip.jsx
+++ app/components/PlanTrip.jsx
@@ -7,15 +7,15 @@ const PlanTrip = () => {
       <div className="mx-auto p-10 max-w-[133rem]">
         <div className="flex flex-col">
           <div className="text-center text-black">
-            <h3 className="regular-20">Plan your trip now</h3>
-            <h2 className="bold-40">Quick & easy car rental</h2>
+            <h3 className="regular-16 lg:regular-20">Plan your trip now</h3>
+            <h2 className="bold-32 lg:bold-40">Quick & easy car rental</h2>
           </div>
 
           <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-auto mt-14 md:gap-y-8 sm:mt-4">
             <div className="plan-trip">
               <Image src="/icon2.png" alt="icon_img" height={150} width={150} />
-              <h3 className="bold-22">Contact Operator</h3>
-              <p className="regular-16">
+              <h3 className="bold-20 lg:bold-22">Contact Operator</h3>
+              <p className="regular-14 lg:regular-16">
                 Our knowledgeable and friendly operators are always ready to
                 help with any questions or concerns
               </p>
@@ -23,8 +23,8 @@ const PlanTrip = () => {
 
             <div className="plan-trip">
               <Image src="/icon1.png" alt="icon_img" height={150} width={150} />
-              <h3 className="bold-22">Select Car</h3>
-              <p className="regular-16">
+              <h3 className="bold-20 lg:bold-22">Select Car</h3>
+              <p className="regular-14 lg:regular-16">
                 We offer a big range of vehicles for all your driving needs. We
                 have the perfect car to meet your needs
               </p>
@@ -32,8 +32,8 @@ const PlanTrip = () => {
 
             <div className="plan-trip">
               <Image src="/icon3.png" alt="icon_img" height={150} width={150} />
-              <h3 className="bold-22">Let's Drive</h3>
-              <p className="regular-16">
+              <h3 className="bold-20 lg:bold-22">Let's Drive</h3>
+              <p className="regular-14 lg:regular-16">
                 Whether you're hitting the open road, we've got you covered with
                 our wide range of cars
               </p>
