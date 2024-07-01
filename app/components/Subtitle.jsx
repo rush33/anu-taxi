1f1aff967708f7e17548133dfaa4a11819ee123b
Anuran Saikia <77756834+nuuron@users.noreply.github.com>
2024-07-01 14:21:45 +0530

    made subtitle text size larger for mobile view

--- app/components/Subtitle.jsx
+++ app/components/Subtitle.jsx
@@ -3,15 +3,15 @@ import { GOOGLE_MAPS, PRIMARY_COLOR, SECONDARY_COLOR } from "../constants";
 
 const Subtitle = () => {
   return (
-    <p className="mt-4 text-gray-600 text-xs sm:text-base">
+    <p className="mt-4 text-gray-600 text-sm sm:text-base">
       Planning a trip to{" "}
-      <span className="font-medium text-xs sm:text-base">Shillong</span>,{" "}
-      <span className="font-medium text-xs sm:text-base">Tawang</span>, or{" "}
-      <span className="font-medium text-xs sm:text-base">Kaziranga</span>? We've
+      <span className="font-medium text-sm sm:text-base">Shillong</span>,{" "}
+      <span className="font-medium text-sm sm:text-base">Tawang</span>, or{" "}
+      <span className="font-medium text-sm sm:text-base">Kaziranga</span>? We've
       got you covered with everything you need for your next adventure in{" "}
-      <span className="font-medium text-xs sm:text-base">Meghalaya</span>,{" "}
-      <span className="font-medium text-xs sm:text-base">Assam</span>, or{" "}
-      <span className="font-medium text-xs sm:text-base">
+      <span className="font-medium text-sm sm:text-base">Meghalaya</span>,{" "}
+      <span className="font-medium text-sm sm:text-base">Assam</span>, or{" "}
+      <span className="font-medium text-sm sm:text-base">
         Arunachal Pradesh
       </span>
       . See our{" "}
@@ -19,13 +19,13 @@ const Subtitle = () => {
         href={GOOGLE_MAPS}
         target="_blank"
         rel="noopener noreferrer"
-        className="font-medium text-xs sm:text-base"
+        className="font-medium text-sm sm:text-base"
         style={{ color: SECONDARY_COLOR }}
       >
         Google reviews
       </a>{" "}
       to find out why we're the{" "}
-      <span className="font-medium text-xs sm:text-base">
+      <span className="font-medium text-sm sm:text-base">
         best service in Guwahati
       </span>
       . Call us now to plan your trip!
